d645b9d1ed5a754eabff402701cd077ef2a60f06
Unnati <unnatikurekar@gmail.com>
2024-02-07 10:46:46 +0530

    Login-Register

---
+++ spring_boot_backend_template/src/main/java/com/app/controller/UserController.java
@@ -0,0 +1,73 @@
+package com.app.controller;
+
+import javax.servlet.http.HttpSession;
+import javax.validation.Valid;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.ui.Model;
+import org.springframework.validation.annotation.Validated;
+import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.app.dto.UserDTO;
+import com.app.entities.Role;
+import com.app.entities.User;
+import com.app.service.UserService;
+
+@RestController
+@RequestMapping("/users")
+@CrossOrigin(origins = "http://localhost:3000")
+@Validated
+public class UserController {
+    @Autowired
+	private UserService userService;
+    
+    
+    @PostMapping("/register")
+	public String addUserDetails(@RequestBody UserDTO userDto) {
+		System.out.println("in add user "+userDto.toString());
+		return userService.addUserDetails(userDto);
+	}
+    
+	@PostMapping("/login")
+	public String processLoginForm(@RequestParam String em,
+			@RequestParam String pass, Model map, HttpSession session) {
+		System.out.println("in process login form " + em + " " + pass);// em pass : not null
+		try {
+			// invoke service layer method for authentication
+			User user = userService.authenticateUser(em, pass);
+			System.out.println(user);
+			// add validated user details under session scope
+			session.setAttribute("emp_details", user);
+			// role based authorization
+			if (user.getRole() == Role.APPLICANT) {
+				// user role , redirect to user details
+				return "redirect:/users/home";
+			}
+			// => admin, trafficpolice role --redirect to dept list page
+			return "redirect:/users/list";
+		} catch (Exception e) {
+			System.out.println("err in handler " + e);
+			// forward clnt in the same request to the login page , highlighted with errs
+			map.addAttribute("message", "Invalid Email or Passsword!!!");
+			return "/login";
+		}
+
+	}
+	
+	
+	@GetMapping("/logout")
+	public String logout(Model map, HttpSession session) {
+			System.out.println("in logout");
+			// add user details under model attribute
+			map.addAttribute("user_details", session.getAttribute("user_details"));
+			// invalidate Http Session
+			session.invalidate();
+			return "/users/logout";
+		}
+}
---
+++ spring_boot_backend_template/src/main/java/com/app/service/UserServiceImpl.java
@@ -0,0 +1,36 @@
+package com.app.service;
+
+import org.modelmapper.ModelMapper;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+import com.app.dao.UserDao;
+import com.app.dto.UserDTO;
+import com.app.entities.User;
+@Service
+@Transactional
+public class UserServiceImpl implements UserService {
+@Autowired
+private UserDao userDao;
+@Autowired
+private UserDTO userDto;
+@Autowired
+private ModelMapper mapper;
+
+@Override
+public User authenticateUser(String em, String pass) {
+
+	return  userDao.findByEmailAndPassword(em, pass).orElseThrow();
+}
+
+@Override
+public String addUserDetails( UserDTO userdto) {
+	System.out.println("in service"+userdto.toString());
+	User user = mapper.map(userdto, User.class);
+	userDao.save(user);
+	return "Added new user ....";
+}
+
+
+}
